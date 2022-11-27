0d3675042a3f2ded4333831fd8cc66ed6008d409
Jotch-96 <107109450+Plasma-Linux@users.noreply.github.com>
2022-11-27 09:42:02 +0900

    全自動で実行されるよう修正

--- deps.sh
+++ deps.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt install binutils debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools syslinux-common isolinux
+sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
+sudo apt install -y　\
+binutils \
+debootstrap \
+squashfs-tools \
+xorriso \
+grub-pc-bin \
+grub-efi-amd64-bin \
+mtools \
+syslinux-common \
+isolinux
 
 exit0
