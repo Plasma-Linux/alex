b395305bc0e6aa358fc834ca50b0de967b080e7b
Jotch-96 <107109450+Plasma-Linux@users.noreply.github.com>
2022-12-16 16:56:35 +0900

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+arch=""
+ubuntu_code_name=""
+ubuntu_ver=""
+ubuntu_repo_url=""
+debootstrap_lebel=""
+os_name=""
+os_ver=""
+os_code_name=""
+iso_name=""
+
+exit0
---
+++ final-process.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#########################################
+#                                       #
+# Welcome to final-process.sh!          #
+# Now let's finish building the system! #
+# We will now run finalprocess.sh.      #
+# Please wait a moment.                 #
+#                                       #
+#########################################
+
+exit0
